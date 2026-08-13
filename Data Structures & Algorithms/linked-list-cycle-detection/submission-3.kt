b78387bd487de3809dc7a32701afd66a7fcf36c1
Diogo Ribeiro <108695074+Ribas13@users.noreply.github.com>
2026-08-13 11:39:23 +0100

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.kt
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        if (head == null) return false
+        var node = head
+        var nodeB = head?.next
+        while (node != null && nodeB != null) {
+            node = node.next
+            nodeB = nodeB.next?.next
+            if (node == nodeB)
+                return true
+        }
+        return false
+    }
+}
